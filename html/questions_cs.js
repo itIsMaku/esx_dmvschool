ddaf85e315e8d9c55eef8598cb2fb95b12b29072
itIsMaku <68100107+itIsMaku@users.noreply.github.com>
2020-09-14 19:21:01 +0000

    Create questions_cs.js

---
+++ html/questions_cs.js
@@ -0,0 +1,91 @@
+var tableauQuestion = [
+	{
+		question: "Pokud jedeš rychlostí 80 km/h a vjíždíš do obydlené oblasti, musíš:",
+		propositionA: "Zvýšit rychlost",
+		propositionB: "Zachováš rychlost, pokud si to situace vyžádá pustíš ostatní vozidla",
+		propositionC: "Zpomalit na 50km/h",
+		propositionD: "Dávat pozor na lidi",
+		reponse: "C"
+	},
+
+	{
+		question: "Pokud odbočuješ doprava na semaforu, máš zelenou ale zrazu ti tam vejde chodec co uděláš?:",
+		propositionA: "Zastavíš v křižovatce dojdeš zbít chodce",
+		propositionB: "Přejedeš chodce protože máš zelenou a jedeš dál",
+		propositionC: "Zastavíš / zpomalíš vozidlo a necháš chodce přejít",
+		propositionD: "Vystřelíš po chodci aby si uvědomil že tudy nemá chodit",
+		reponse: "C"
+	},
+
+	{
+		question: "Pokud značka neurčí jinak, je povolená rychlost v obydlených oblastech: __ km/h",
+		propositionA: "30 km/h",
+		propositionB: "50 km/h",
+		propositionC: "40 km/h",
+		propositionD: "60 km/h",
+		reponse: "B"
+	},
+
+	{
+		question: "Pokaždé pokud vjíždíš do jiného pruhu musíš:",
+		propositionA: "Zkontrolovat zrcátka",
+		propositionB: "Zkontrolovat slepá místa",
+		propositionC: "Dát blinkr",
+		propositionD: "Vše výše uvedené",
+		reponse: "D"
+	},
+
+	{
+		question: "Jaká je maxiální hranice alkoholu v krvi při řízení motorového vozidla?",
+		propositionA: "0.05%",
+		propositionB: "0.18%",
+		propositionC: "0.08%",
+		propositionD: "0.06%",
+		reponse: "C"
+	},
+
+	{
+		question: "Kdy můžeš pokračovat v jízdě na semaforech?",
+		propositionA: "Když je zelená",
+		propositionB: "Když se nikdo nekouká",
+		propositionC: "Když svítí červená",
+		propositionD: "Když svítí zelená a na červenou lze odbočit doprava",
+		reponse: "D"
+	},
+
+	{
+		question: "Co uděláte pokud ve zpětném zrcátku zahlédnete blikající policejní auto?",
+		propositionA: "Ujedete rychle pryč",
+		propositionB: "Začnete střílet do světel",
+		propositionC: "Zapnete výstražná světla a jedete dále",
+		propositionD: "Zastavíte na krajnici a vyčkáte na příchod policisty",
+		reponse: "D"
+	},
+
+	{
+		question: "Co je povolené při předjíždění jiného vozidla?",
+		propositionA: "Jedete těsně za něho, zvýšíte rychlost a co nejrychleji ho objedete zprava",
+		propositionB: "Předjedete ho zprava, protože nelze vjet do protisměru",
+		propositionC: "Předjedete ho z bezpečné vzdálenosti od něho, můžete při tom zvýšit rychlost a musíte dát znamení o změně směru jízdy",
+		propositionD: "Držíte rychlost a až on zpomalí předjedete ho",
+		reponse: "C"
+	},
+
+	{
+		question: "Jedete po dálnici, kde vozidla jedou 130 km/h přitom povolených je 120km/h, kolik pojedete vy?:",
+		propositionA: "120 km/h",
+		propositionB: "125 km/h",
+		propositionC: "130 km/h",
+		propositionD: "110 km/h",
+		reponse: "A"
+	},
+
+	{
+		question: "Pokud vás předjíždí jiné vozidlo, tak v žádném případě NESMÍTE:",
+		propositionA: "Zpomalit",
+		propositionB: "Zkontrolovat zrcátka",
+		propositionC: "Sledovat ostatní co dělají",
+		propositionD: "Zrychlovat",
+		reponse: "D"
+	},
+]
